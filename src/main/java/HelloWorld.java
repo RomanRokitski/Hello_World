05ba40cf9be55a72dabf7375f3e9d1772cfbdb68
RomanRokitski <romanrokitski@gmail.com>
2022-07-08 00:20:18 +0300

    Changed HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello GOIT World");
         System.out.println("This is our second commit");
+        System.out.println("This is our third commit");
+        System.out.println("This is our fourth commit");
     }
 }
